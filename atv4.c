46791579bfb9eb05394baa8a9cf0c99902913612
Raniel <whyfck@gmail.com>
2017-07-21 01:00:03 -0300

    Update atv4.c

--- atv4.c
+++ atv4.c
@@ -7,7 +7,7 @@ int main(void)
     char estc[100], filho[100];
 
     printf("Qual seu salario?\n");
-    scanf("%lf",&sal);
+    scanf("%f",&sal);
     printf("Voce e casada? Responda com sim ou nao.\n");
     scanf("%s",estc);
     printf("Tem filhos? Responda com sim ou nao.\n");
@@ -19,22 +19,19 @@ int main(void)
         ttl= sal+aux;
 
         printf("\n");
-        printf("Voce recebera um auxilio de R$: ""%f",aux);
-        printf("\n");
-        printf("Seu salario ficou: R$: ""%f",ttl);
+        printf("Você receberá um auxílio de R$: %.2f (35%%). Seu salário ficou: R$ %.2f",aux,ttl);
+        
     } else if (strcmp (estc,"sim")==0 && strcmp (filho,"nao")==0)
     {
             aux= sal*15/100;
             ttl= sal+aux;
 
             printf("\n");
-            printf("Voce recebera um auxilio de R$: ""%f",aux);
-            printf("\n");
-            printf("Seu salario ficou: R$: ""%f",ttl);
+            printf("Você receberá um auxílio de R$: %.2f (15%%). Seu salário ficou: R$ %.2f",aux,ttl);
     } else
     {
         printf("\n");
-        printf("Voce nao recebera auxilio.");
+        printf("Você não receberá auxílio.");
     }
 
 }
