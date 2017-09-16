62924e573600ca08733dd52a9fceb6a861c5d3b9
ranielcsar <whyfck@gmail.com>
2017-09-16 12:37:39 -0300

    correção de português

--- While/farenheit_celsius.c
+++ While/farenheit_celsius.c
@@ -5,12 +5,12 @@ int main(void)
     float i=50,c;
     
     
-    printf("Tabela de graus Farenheit em Celcius, de 50 a 65.\n\n");
+    printf("Tabela de graus Farenheit em Celsius, de 50 a 65.\n\n");
     
     do
     {
         c=(i-32)/1.8;
-        printf("Farenheit: %.f = Celcius: %.2f\n",i,c);
+        printf("Farenheit: %.f = Celsius: %.2f\n",i,c);
         i++;
     } while(i<=65);
     
