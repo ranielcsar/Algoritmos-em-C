291a2cf0aa6b34578451ec970940fae31a822ffc
ranielcsar <whyfck@gmail.com>
2017-09-25 11:24:49 -0300

    lógica da validação agora em uma função

--- Validador-CPF.c
+++ Validador-CPF.c
@@ -3,17 +3,12 @@
 #include <locale.h>
 #include <string.h>
 
-int main()
+int validaCPF(char cpf[])
 {
-    setlocale(LC_ALL,"portuguese");
-    char cpf[15];
-    int icpf[15];
-    int mPdigito,sPdigito,diviP,pDigi; //VÃ¡riaveis usadas para validaÃ§Ã£o do 1Âº digito
-    int mSdigito,sSdigito=0,diviS,sDigi; //VÃ¡riaveis usadas para validaÃ§Ã£o do 2Âº digito
-    int p=10,s=11,i; //contadoras
-
-    printf("Entre com seu CPF (111.111.111-11): ");
-    scanf("%s",cpf);
+	int icpf[15];
+    int mPdigito,sPdigito=0,diviP,pDigi; //Váriaveis usadas para validação do 1º digito
+    int mSdigito,sSdigito=0,diviS,sDigi; //Váriaveis usadas para validação do 2º digito
+    int p=10,s=11,i; //contadoras    
 
     for (i=0; i<strlen(cpf);i++)
     {
@@ -26,8 +21,8 @@ int main()
 
         if (p>1)
         {
-            mPdigito=icpf[i]*p; //multiplicaÃ§Ã£o de cada nÃºmero do cpf
-            sPdigito+=mPdigito; //soma de todos os resultados do cÃ¡lculo acima
+            mPdigito=icpf[i]*p; //multiplicação de cada número do cpf
+            sPdigito+=mPdigito; //soma de todos os resultados do cálculo acima
             p--;
         }
         if (s>1)
@@ -37,9 +32,10 @@ int main()
             s--;
         }
     }
+    
     diviP=sPdigito%11;
     pDigi=11-diviP;
-    diviS=sSdigito%11;
+    diviS=(sSdigito%11);
     sDigi=11-diviS;
 
     if (diviS<2)
@@ -49,15 +45,35 @@ int main()
     if (diviP<2)
     {
         sDigi=0;
-    }
+    }    
 
     if ((pDigi==icpf[12]) && (sDigi==icpf[13]))
     {
-        printf("CPF VÃ¡lido!\n");
+        return 0;
     } else
     {
-        printf("CPF invÃ¡lido!\n");
+        return 1;
     }
+}
 
+int main()
+{
+    setlocale(LC_ALL,"");
+    char cpf[15];
+	int valida;    
+
+    printf("Entre com seu CPF (111.111.111-11): ");
+    scanf("%s",cpf);    
+    
+	valida=validaCPF(cpf);
+    
+    if (valida==0)
+    {
+    	printf("CPF válido.\n\n");
+	} else
+	{
+		printf("CPF inválido.\n\n");
+	}
+	    
     system("pause");
 }
