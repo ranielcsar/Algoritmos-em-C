f2ce4b4fd77f8ab0188cefdc6997aae4715105d1
Raniel <whyfck@gmail.com>
2017-10-16 15:43:42 -0300

    Update bubbleSort.c

--- Ordenação/bubbleSort.c
+++ Ordenação/bubbleSort.c
@@ -7,8 +7,8 @@ int* ordenacaoBolha(int num[]) //Bubble sort
 	int i, j, troca;
 	static int x[30];
 	
-	for (i=0;i<10;i++){
-		for (j=i+1;j<10;j++)
+	for (i=0; i<10; i++){
+		for (j=i+1; j<10; j++)
 		{
 			if (num[i] > num[j])
 			{
