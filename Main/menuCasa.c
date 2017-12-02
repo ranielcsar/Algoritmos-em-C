51472f4306293f46a9e164132665e6e7dcbc094e
Raniel <whyfck@gmail.com>
2017-12-01 21:32:33 -0300

    Desenho para ser usado como menu! :D

---
+++ Main/menuCasa.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+main() {
+   
+   printf("               ( \n");
+   printf("                  ) \n");
+   printf("               (            ./\\. \n");
+   printf("            |^^^^^^^^^|   ./LLLL\\. \n");
+   printf("            |`.'`.`'`'| ./LLLLLLLL\\. \n");
+   printf("            |.'`'.'`.'|/LLLL/^^\\LLLL\\. \n");
+   printf("            |.`.''``./LLLL/^ () ^\\LLLL\\. \n");
+   printf("            |.'`.`./LLLL/^  =   = ^\\LLLL\\. \n");
+   printf("            |.`../LLLL/^  _.----._  ^\\LLLL\\. \n");
+   printf("            |'./LLLL/^                ^\\LLLL\\. \n");
+   printf("            |/LLLL/^                    ^\\LLLL\\. \n");
+   printf("          ./LLLL/^                        ^\\LLLL\\. \n");
+   printf("        ./LLLL/^                            ^\\LLLL\\. \n");
+   printf("      ./LLLL/^                                ^\\LLLL\\. \n");
+   printf("    ./LLLL/^                                    ^\\LLLL\\. \n");
+   printf("   /LLLL/^                                        ^\\LLLL\\ \n");
+   printf("   ~~|.~                                            ~.|~~ \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     ||                                              || \n");
+   printf("     |------------------------------------------------| \n");
+}
