bbebaa667788a91bfaf5d7b7220f46b5f62bf8bc
Raniel <whyfck@gmail.com>
2017-12-20 20:20:36 -0300

    Ordenando struct :D

---
+++ Ordenação/Ordenação de struct.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define max 5
+
+typedef struct
+{
+    int elementoUm;
+    char nome[30];
+} teste;
+
+void geraNumeros(teste *t)
+{
+    int i;
+
+    printf("Entre com os nomes > \n\n");
+    
+    for (i=0; i<max; i++)
+    {
+        printf("%dº nome: ", i+1);
+        scanf("%s", t[i].nome);
+        t[i].elementoUm= rand()%10;
+    }
+    printf("\n\n");
+}
+
+void ordena(teste *t)
+{
+    int i, j;
+    teste aux;
+    
+    for (i=0; i<max; i++) {
+        for (j=0; j<max; j++)
+        {
+            if (t[i].elementoUm < t[j].elementoUm)
+            {
+                aux= t[i];
+                t[i]= t[j];
+                t[j]= aux;
+            }
+        }
+    }
+    
+    printf("*- Elementos ordenados -* \n");
+    
+    for (i=0; i<max; i++)
+    {
+        printf("Nome: %s \n", t[i].nome);
+        printf("Número: %d \n\n", t[i].elementoUm);
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    
+    teste T[max];
+    
+    geraNumeros(T);
+    
+    ordena(T);
+}
